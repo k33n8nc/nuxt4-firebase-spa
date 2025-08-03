6c48a8d1a52a1e4bafb6cf061fb9855e08d1b043
k33n8nc <robbert.stapel@live.nl>
2025-08-03 04:15:48 +0200

    Add breadcrumbs to new pages

--- app/pages/calendar.vue
+++ app/pages/calendar.vue
@@ -3,3 +3,18 @@
     Calendar page
   </div>
 </template>
+
+<script setup lang="ts">
+import { useBreadcrumbs } from '~/composables/useBreadcrumbs';
+
+definePageMeta({
+  middleware: 'auth',
+});
+
+const { setBreadcrumbs } = useBreadcrumbs();
+
+setBreadcrumbs([
+  { text: 'Dashboard', to: '/' },
+  { text: 'Calendar', to: '/calendar' },
+]);
+</script>
--- app/pages/registrations/index.vue
+++ app/pages/registrations/index.vue
@@ -3,3 +3,18 @@
     Registrations page
   </div>
 </template>
+
+<script setup lang="ts">
+import { useBreadcrumbs } from '~/composables/useBreadcrumbs';
+
+definePageMeta({
+  middleware: 'auth',
+});
+
+const { setBreadcrumbs } = useBreadcrumbs();
+
+setBreadcrumbs([
+  { text: 'Dashboard', to: '/' },
+  { text: 'Registrations', to: '/registrations' },
+]);
+</script>
