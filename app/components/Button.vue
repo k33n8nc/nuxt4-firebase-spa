724b51caa57088feb783026e8059a269474cdf81
k33n8nc <robbert.stapel@live.nl>
2025-08-08 00:00:38 +0200

    Change UI

--- app/components/Button.vue
+++ app/components/Button.vue
@@ -1,7 +1,7 @@
 <template>
   <button
       type="button"
-      class="bg-orange-500 rounded-md h-10 px-[14px] cursor-pointer text-white text-sm inline-flex items-center"
+      class="bg-gray-800 rounded-md h-10 px-[14px] cursor-pointer text-white text-sm inline-flex items-center"
   >
     <slot />
   </button>
