2c81726eab725217c5f17f0d0be0cae0b782d861
k33n8nc <robbert.stapel@live.nl>
2025-10-19 01:20:41 +0200

    Better login

--- app/pages/login.vue
+++ app/pages/login.vue
@@ -1,16 +1,19 @@
 <template>
   <div class="flex flex-col items-center justify-center bg-gray-100 h-screen overflow-y-auto">
-    <h1 class="font-medium text-center mb-4">Login to Paul van Dam</h1>
+    <div class="w-md bg-white rounded-md shadow mb-4 px-6 py-4">
+      <h2 class="text-xl">Cybernetix</h2>
+      <p class="font-light text-sm">Make. Advanced. Simple.</p>
+    </div>
     <div class="w-md mx-auto border-gray-200 shadow p-8 rounded-lg bg-white">
       <form @submit.prevent="login">
         <div class="space-y-4">
           <div>
             <label for="email" class="block text-sm font-medium text-gray-700">Email</label>
-            <input v-model="email" type="email" id="email" required class="w-full px-3 py-2 mt-1 border-1 border border-gray-200 rounded-md">
+            <input v-model="email" type="email" id="email" required class="w-full px-3 py-2 mt-1 border border-gray-200 rounded-md focus:outline-none focus:border-gray-800 autofill:!bg-red-200">
           </div>
           <div>
             <label for="password" class="block text-sm font-medium text-gray-700">Password</label>
-            <input v-model="password" type="password" id="password" required class="w-full px-3 py-2 mt-1 border border-gray-200 rounded-md">
+            <input v-model="password" type="password" id="password" required class="w-full px-3 py-2 mt-1 border border-gray-200 rounded-md focus:outline-none focus:border-gray-800 autofill:!bg-yellow-200">
           </div>
         </div>
         <div class="mt-6 flex justify-between items-center">
@@ -47,3 +50,21 @@ const login = async () => {
   }
 };
 </script>
+
+<style>
+
+
+input:-webkit-autofill {
+  border: 1px solid #e5e7eb;
+  border-radius: .3rem;
+  caret-color: #1e2939; /* Pour le I quand on édite */
+  color: #1e2939;
+  background: #e5e7eb;
+  /* webkit autofill */
+  -webkit-text-fill-color: #1e2939; /* Surcharge la font color d'autofill */
+  -webkit-background-clip: text; /* Supprime le background autofill, utile pour le border radius */
+  box-shadow: 0 0 0 50px #e5e7eb inset; /* Ajoute un fake background à base d'ombrage aplatit */
+}
+
+
+</style>
